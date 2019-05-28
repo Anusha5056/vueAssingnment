32ab0f1dcc94c14576e728cf169bc206af8842a8
Anusha5056 <anusha.udathala@fissionlabs.com>
2019-05-28 13:10:27 +0530

    task

---
+++ App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id = "app">
+    <Tree :node="root"/>
+  </div>
+</template>
+<script>
+import Tree from './components/Tree';
+export default {
+  components: {
+    Tree
+  },
+  name: 'app',
+  data(){
+    return {
+      root:
+      {
+        name: 'show',
+        children:[{
+              name:'furniture',
+              children:[
+                {name: 'srk',children:[{name:'sofa',children:[{name:'2'},{name:'3'}]},{name:'chair',children:[{name:'2'},{name:'3'}]}]},
+                {name: 'br',children:[{name:'dt',children:[{name:'2'},{name:'3'}]},{name:'bd',children:[{name:'2'},{name:'3'}]}]}]
+        },
+        {
+              name:'electronics',
+              children:[
+                {name: 'mobile',children:[{name:'samsung',children:[{name:'sam1'},{name:'sam2'}]},{name:'vivo',children:[{name:'vivoy81'},{name:'vivopro'}]}]},
+                {name: 'laptops',children:[{name:'lenovo',children:[{name:'lenovo1'},{name:'lenovo i7'}]},{name:'apple',children:[{name:'macbook1'},{name:'macbook2'}]}]}]
+        },
+        {
+              name:'cosmetics',
+              children:[
+                {name: 'revlon',children:[{name:'mb',children:[{name:'mb1'},{name:'mb2'}]},{name:'el',children:[{name:'el1'},{name:'el2'}]}]},
+                {name: 'dazler',children:[{name:'mb',children:[{name:'mb1'},{name:'mb2'}]},{name:'el',children:[{name:'el1'},{name:'el2'}]}]}]
+        },
+                
+              ]
+        
+      }
+    }
+  }
+}
+</script>
---
+++ Tree.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div @click = "expanded = !expanded"  class = "node">
+            <p align = "center">{{node.name}}</p>
+         </div>
+         <Tree style="padding-left:100px" v-if = "expanded" v-for="(child,index) in node.children" :key = "index"
+                :node ="child"
+                :depth = "depth+1"/>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Tree',
+    props: {
+        node: Object,
+        depth: {
+            type: Number,
+            default:0
+        }
+    },
+    data(){
+        return {
+            expanded: false,
+        }
+        
+    
+    }
+}
+</script>
+<style>
+.pad{
+    padding-left: 40px;
+}
+</style>
